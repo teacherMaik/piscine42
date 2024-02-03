edd507ec02a0f79d2c215a20dd30b27b213fc07f
teacherMaik <d.michaelthomasbennett@gmail.com>
2024-02-03 22:06:21 +0100

    Duplicate Shell00 subject eliminated

--- C00_try_4/ex05/ft_print_comb.c
+++ C00_try_4/ex05/ft_print_comb.c
@@ -14,23 +14,23 @@
 
 void	ft_print_comb(void)
 {
-	int	i;
-	int	j;
-	int	k;
+	int	i; //1st digit of number
+	int	j; //2cnd digit of number
+	int	k; //3rd digit of number
 
-	i = 47;
-	while (++i <= 55 )
+	i = 47; //start first digit at ASCII previous to "0"
+	while (++i <= 55 ) //Up "i" before the logical comparison
 	{
-		j = i;
-		while (++j <= 56)
+		j = i; //Set "j" to "i"
+		while (++j <= 56) //Up "j" before the logical comparison so it is 1 more than "i"
 		{
-			k = j;
-			while (++k <= 57)
+			k = j; //Set "k" to "j"
+			while (++k <= 57) //Up "k" before the logical comparison so it is 1 more than "j" and 2 more than "i"
 			{
-				write (1, &i, 1);
+				write (1, &i, 1); //Three writes to print the 3 digit number on the terminal line
 				write (1, &j, 1);
 				write (1, &k, 1);
-				if (i != 55)
+				if (i != 55)  //checks to see if "i" is 7, which will be last valid number (789). If not, add ", " to terminal line to seperate numbers
 				{
 					write (1, ",", 1);
 					write (1, " ", 1);
